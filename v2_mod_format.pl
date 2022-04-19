cf9378bd5c386c92f0b2b18d6b440be8b27c8b79
triccsegg <triccsegg@gmail.com>
2022-04-19 21:08:55 +0800

    modify size change script

--- v2_mod_format.pl
+++ v2_mod_format.pl
@@ -197,7 +197,7 @@ open OUTF, ">:encoding(UTF-8)", "$output_file_phase"  or die "can't open file $o
     chomp;
 	if($_ !~ /<a\s*href.*?=s\d+\s*"/)
 	{
-		s!(<a\s*href\s*=\s*"https://blogger.googleusercontent.com/img/.*?)".*?>.*?(<img.*?)\s*"\s*/>\s*</a>!$1=s16000">$2=s16000"/></a>!g;
+		s!(<a\s*href\s*=\s*"https://blogger.googleusercontent.com/img/.*?)".*?>.*?(<img.*?)\s*"\s*/>\s*</a>!$1">$2"/></a>!g;
 	}
     print OUTF "$_\n";
   }
@@ -217,10 +217,10 @@ open OUTF, ">:encoding(UTF-8)", "$output_file_phase"  or die "can't open file $o
 	
 	#s!height="640"!!g;
 	#s!width="640"!!g;
-	(my $mod_str = $_) =~ s,=s\d+",=s16000",g; #keep orignal size at $_
-	if($_ =~ m,=s\d+", and $_ !~ m,=s16000",)
+	(my $mod_str = $_) =~ s,/s\d+/,/s1600/,g; #keep orignal size at $_
+	if($_ =~ m,/s\d+/, and $_ !~ m,/s1600/,)
 	{
-	  print LOGF "=== change the size to s16000 ===\n";
+	  print LOGF "=== change the size to s1600 ===\n";
 	  print LOGF "org:$_\n";
 	  print LOGF "mod:$mod_str\n";
 	}
